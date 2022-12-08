8390ad5a995f2e93188028bce6c62b5cf5c88771
Sergey-life <sergei.kharchenko888@gmail.com>
2022-12-09 00:30:41 +0200

    first commit

---
+++ app/src/Admin/PostAdmin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\Category;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelType;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PostAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('title', TextType::class)
+            ->add('body', TextareaType::class)
+            ->add('active', CheckboxType::class)
+            ->add('published', CheckboxType::class)
+            ->add('category', ModelType::class, [
+                'class' => Category::class,
+                'property' => 'name'
+        ])
+            ->add('image', FileType::class, [
+                'required' => false,
+            ]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter->add('title');
+        $filter->add('body');
+        $filter->add('active');
+        $filter->add('published');
+        $filter->add('category');
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list->addIdentifier('title');
+        $list->addIdentifier('body');
+        $list->addIdentifier('active');
+        $list->addIdentifier('published');
+        $list->addIdentifier('category');
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show->add('title');
+        $show->add('body');
+        $show->add('active');
+        $show->add('published');
+        $show->add('category');
+    }
+
+    public function prePersist(object $image): void
+    {
+        $this->manageFileUpload($image);
+    }
+
+    public function preUpdate(object $image): void
+    {
+        $this->manageFileUpload($image);
+    }
+
+    private function manageFileUpload(object $image): void
+    {
+        if ($image->getFile()) {
+            $image->refreshUpdated();
+        }
+    }
+}
---
+++ app/src/Entity/Post.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    const SERVER_PATH_TO_IMAGE_FOLDER = '/public/images';
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $body = null;
+
+    #[ORM\Column(length: 255)]
+    private ?UploadedFile $image = null;
+
+    #[ORM\Column]
+    private ?bool $active = null;
+
+    #[ORM\Column]
+    private ?bool $published = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[Gedmo\Timestampable(on:"update")]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updated = null;
+
+    #[Gedmo\Timestampable(on:"update")]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?UploadedFile $image = null): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdated(): ?\DateTimeInterface
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated(\DateTimeInterface $updated): self
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Manages the copying of the file to the relevant place on the server
+     */
+    public function upload(): void
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getImage()) {
+            return;
+        }
+
+        // we use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and target filename as params
+        $this->getImage()->move(
+            self::SERVER_PATH_TO_IMAGE_FOLDER,
+            $this->getImage()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->filename = $this->getImage()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->setImage(null);
+    }
+
+    /**
+     * Lifecycle callback to upload the file to the server.
+     */
+    public function lifecycleFileUpload(): void
+    {
+        $this->upload();
+    }
+
+    /**
+     * Updates the hash value to force the preUpdate and postUpdate events to fire.
+     */
+    public function refreshUpdated(): void
+    {
+        $this->setUpdated(new \DateTime());
+    }
+
+    // ... the rest of your class lives under here, including the generated fields
+    //     such as filename and updated
+}
